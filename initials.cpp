7a6fea6045b9d3538bb282a2af7be06dc66c2af1
abhishek98765 <42344548+abhishek98765@users.noreply.github.com>
2019-05-14 07:23:25 +0530

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+/*
+                  ^
+                 ^^^
+                ^^^^^
+               ^^^^^^^
+              ^^^^^^^^^
+             ^^^^^^^^^^^
+            ^^^^^^^^^^^^^
+		   ^^^  0   0  ^^^
+		  ^^^     J     ^^^
+		 ^^^   <----->   ^^^
+		^^^	              ^^^
+											*/
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
+#define ll         	long long int
+#define ull			unsigned long long int
+#define mp         	make_pair
+#define pb         	push_back
+#define eb		   	emplace_back
+#define f(i,a,b)   	for(int i=a;i<b;i++)
+#define rf(i,a,b)  	for(int i=a-1;i>=b;i--)
+#define F          	first
+#define S          	second
+#define vi         	vector<int>
+#define pii        	pair<int,int>
+#define all(p)     	p.begin(),p.end()
+#define max(x,y)   	(x>y)?x:y
+#define min(x,y)   	(x<y)?x:y
+#define mid(x,y)   	(x+(y-x)/2)
+#define mini       	INT_MIN
+#define hello      	ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define coder   	int t; cin>>t; tc(t);
+#define nod(a) 		floor(log10(a))+1; 
+#define pow2(a)		a&&(!(a&(a-1)));
+#define copy(a,b,n) copy_n(a,n,b);
+#define mset(a,b,c) f(i,0,b) a[i]=c
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
+
+void tc(int t)
+{
+	f(i,0,t)
+	{
+		
+	}
+}
+int main()
+{
+	hello coder
+	return 0;
+}
