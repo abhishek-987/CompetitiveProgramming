63afbf20e715eda34ab326054297c6da29e3dd4c
Abhishek <42344548+abhishek-987@users.noreply.github.com>
2019-06-25 12:19:40 +0530

    Update initials.cpp
    updated

--- initials.cpp
+++ initials.cpp
@@ -1,53 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define fore(i, a, b) for (int i = (int)(a); i <= (int)(b); ++i)
 
 using namespace std;
-/*
-                  ^
-                 ^^^
-                ^^^^^
-               ^^^^^^^
-              ^^^^^^^^^
-             ^^^^^^^^^^^
-            ^^^^^^^^^^^^^
-		   ^^^  0   0  ^^^
-		  ^^^     J     ^^^
-		 ^^^   <----->   ^^^
-		^^^	              ^^^
-											*/
-/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
-#define ll         	long long int
-#define ull			unsigned long long int
-#define mp         	make_pair
-#define pb         	push_back
-#define eb		   	emplace_back
-#define f(i,a,b)   	for(int i=a;i<b;i++)
-#define rf(i,a,b)  	for(int i=a-1;i>=b;i--)
-#define F          	first
-#define S          	second
-#define vi         	vector<int>
-#define pii        	pair<int,int>
-#define all(p)     	p.begin(),p.end()
-#define max(x,y)   	(x>y)?x:y
-#define min(x,y)   	(x<y)?x:y
-#define mid(x,y)   	(x+(y-x)/2)
-#define mini       	INT_MIN
-#define hello      	ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define coder   	int t; cin>>t; tc(t);
-#define nod(a) 		floor(log10(a))+1; 
-#define pow2(a)		a&&(!(a&(a-1)));
-#define copy(a,b,n) copy_n(a,n,b);
-#define mset(a,b,c) f(i,0,b) a[i]=c
-/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
 
-void tc(int t)
-{
-	f(i,0,t)
-	{
-		
-	}
-}
-int main()
-{
-	hello coder
-	return 0;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef long long i64;
+typedef vector<i64> vi64;
+typedef vector<vi64> vvi64;
+typedef pair<i64, i64> pi64;
+typedef vector<pi64> vpi64;
+typedef double ld;
+
+template<class T> bool uin(T &a, T b) { return a < b ? (a = b, true) : false; }
+template<class T> bool uax(T &a, T b) { return a > b ? (a = b, true) : false; }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+    cout << fixed;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "rt", stdin);
+#endif
+
+
+
+
+
+#ifndef ONLINE_JUDGE
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+
+    return 0;
 }
