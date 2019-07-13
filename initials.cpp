1e324a03a35427bca0449ba8d3897a3e859b6c64
Abhishek <42344548+abhishek-987@users.noreply.github.com>
2019-07-13 13:02:24 +0530

    Update initials.cpp

--- initials.cpp
+++ initials.cpp
@@ -5,30 +5,10 @@
 #define eb emplace_back
 #define um unordered_map
 #define all(x) (x).begin(), (x).end()
-#define foru(i, n) for (int i = 0; i < (int)(n); ++i)
-#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
-#define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define forn(i,n) for(int i=0;i<(int)(n);++i)
+#define for1(i,n) for(int i=1;i<=(int)(n);++i)
 
 using namespace std;
-
-void check(int num){
-	cerr << "error" << " " << num <<endl;
-}
-void Solve();
-
-int32_t main() {
-#ifndef ONLINE_JUDGE
-	ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.precision(10);
-    cout << fixed;
-   	freopen("input.txt", "rt", stdin);
-    Solve();
-    cerr << "Time: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
-#endif
-	return 0;
-}
-
 
 typedef long double ld;
 typedef long long ll;
@@ -44,7 +24,24 @@ template<class T>bool uax(T &a, T b){ return a<b?(a = b, true): false; }
 
 const ll MOD = 1e9+7, INF = 1e18;
 
-void Solve(){
+void check(int num){
+	cerr << "error" << " " << num <<endl;
+}
 
+int32_t main() {
 	
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+    cout << fixed;
+#ifndef ONLINE_JUDGE
+	
+	freopen("input.txt", "rt", stdin);
+#endif
+	
+	
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+	return 0;
 }
