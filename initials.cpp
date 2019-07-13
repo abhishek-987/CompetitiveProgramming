b055219ec17ac742f4a694102259c7a57038a5fc
Abhishek <42344548+abhishek-987@users.noreply.github.com>
2019-07-13 11:40:08 +0530

    Update initials.cpp

--- initials.cpp
+++ initials.cpp
@@ -1,47 +1,57 @@
 #include <bits/stdc++.h>
 
-#define mp make_pair
-#define mt make_tuple
 #define fi first
 #define se second
-#define pb push_back
 #define eb emplace_back
+#define um unordered_map
 #define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define foru(i, n) for (int i = 0; i < (int)(n); ++i)
 #define for1(i, n) for (int i = 1; i <= (int)(n); ++i)
 #define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
-#define fore(i, a, b) for (int i = (int)(a); i <= (int)(b); ++i)
 
 using namespace std;
 
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<pii> vpi;
-typedef vector<vi> vvi;
-typedef long long ll;
-typedef vector<ll> vll;
-typedef vector<vll> vvll;
-typedef pair<ll, ll> pll;
-typedef vector<pll> vpll;
-typedef long double ld;
+void check(int num){
+	cerr << "error" << " " << num <<endl;
+}
+void Solve();
 
-int main() {
-    ios::sync_with_stdio(false);
+int32_t main() {
+#ifndef ONLINE_JUDGE
+	ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
     cout << fixed;
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "rt", stdin);
+   	freopen("input.txt", "rt", stdin);
+    Solve();
+    cerr << "Time: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
 #endif
+	return 0;
+}
 
 
+typedef long double ld;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vpi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pair<ll, ll>> vpll;
 
+template<class T>bool uin(T &a, T b){ return a>b ? (a = b, true) : false; }
+template<class T>bool uax(T &a, T b){ return a<b?(a = b, true): false; }
 
+const ll MOD = 1e9+7, INF = 1e18;
 
-#ifndef ONLINE_JUDGE
-    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
-#endif
+void Solve(){
 
-    return 0;
+	int t;	
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		cout<<n*5<<endl;
+	}
+
 }
