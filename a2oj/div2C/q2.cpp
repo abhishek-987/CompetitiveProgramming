05aa57fd74b98a64fb7f8c5cd2ea13f11ed65658
Abhishek Chaturvedi <abhishekchaturvedi987@gmail.com>
2019-07-30 17:45:40 +0530

    updated a2oj

--- a2oj/div2C/q2.cpp
+++ a2oj/div2C/q2.cpp
@@ -15,8 +15,7 @@ template<class T>bool uin(T &a,T b){return a>b?(a=b,true):false;}
 template<class T>bool uax(T &a,T b){return a<b?(a=b,true):false;}
 const ll MOD=1e9+7,INF=1e18;
 /********************************************************************************/
-int a[1000010];
-long long cnt[1000010];
+ll a[1000000], cnt[1000000];
 
 int32_t main(){
     ios::sync_with_stdio(false);cin.tie(nullptr);
@@ -25,35 +24,19 @@ int32_t main(){
     freopen("input.txt","rt",stdin);
 #endif
 
-    int n;
-    cin>>n;
-    ll s = 0;
-    forn(i,n){
-        cin>>a[i];
-        s+=a[i];
+    ll n,s=0,ans=0; cin>>n;
+    forn(i,n) cin>>a[i], s+=a[i];
+    if(s%3!=0) cout<<"0"<<endl;
+    else{
+        ll ss=0; s/=3;
+        for(int i=n-1;i>=0;i--){ ss+=a[i]; if(ss==s) cnt[i]=1; }
+        for(int i=n-2;i>=0;i--) cnt[i]+=cnt[i+1];
+        ss=0, ans=0;
+        forn(i,n-2){ ss+=a[i]; if(ss==s) ans+=cnt[i+2]; } 
+        cout<<ans<<endl;
     }
-    if (s%3!=0)
-        cout<<"0"<<endl;
-    else {
-        s/=3;
-        ll ss=0;
-        for(int i=n-1;i>=0;--i){
-            ss+=a[i];
-            if(ss==s)
-                cnt[i]=1;
-        }
-        for(int i=n-2;i>=0;--i)
-            cnt[i]+=cnt[i+1];
-        
-        ll ans=0;
-        ss=0;
-        for(int i=0;i+2<n;++i){
-            ss+=a[i];
-            if(ss==s)
-                ans+=cnt[i+2];
-        }
-        cout<<ans<<"\n";
-    }
+    
+
 
 #ifndef ONLINE_JUDGE
     cerr<<"Time:"<<1.0*clock()/CLOCKS_PER_SEC<<"s.\n";
