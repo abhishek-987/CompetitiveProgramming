63e68c5e9201f0cebfe59e6bd5d3208e653e2428
Abhishek Chaturvedi <abhishekchaturvedi987@gmail.com>
2019-12-23 08:59:21 +0530

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -4,33 +4,33 @@
 #define eb emplace_back
 #define um unordered_map
 #define all(x) (x).begin(),(x).end()
-#define forn(i, n) for(int i = 0; i < int(n); ++i)
-#define for1(i, n) for(int i = 1; i <= int(n); ++i)
-#define ford(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
-#define fore(i, a, b) for (int i = (int)(a); i <= (int)(b); ++i)
-#define che(x) cout << #x << " having " << x << endl 
+#define forn(i,n) for(int i=0;i<int(n);++i)
+#define for1(i,n) for(int i=1;i<=int(n);++i)
+#define ford(i,n) for(int i=(int)(n)-1;i>=0;--i)
+#define fore(i,a,b) for(int i=(int)(a);i<=(int)(b);++i)
+#define che(x) cout<<#x<<" having "<<x<<endl 
 using namespace std;
-typedef long double ld; typedef long long ll;
-typedef vector<int> vi; typedef vector<pair<int, int>> vpi; typedef vector<vi> vvi;
-typedef vector<ll> vll; typedef vector<pair<ll, ll>> vpll; typedef vector<vll> vvll;
-template<class T>bool uin(T &a, T b) {return a > b ? (a = b, true) : false; }
-template<class T>bool uax(T &a, T b) {return a < b ? (a = b, true) : false; }
-const ll MOD = 1e9+7, INF = 1e18;
+typedef long double ld;typedef long long ll;
+typedef vector<int>vi;typedef vector<pair<int,int>>vpi;typedef vector<vi>vvi;
+typedef vector<ll>vll;typedef vector<pair<ll,ll>>vpll;typedef vector<vll>vvll;
+template<class T>bool uin(T &a,T b){return a>b?(a=b,true):false;}
+template<class T>bool uax(T &a,T b){return a<b?(a=b,true):false;}
+const ll MOD=1e9+7, INF=1e18;
 /********************************************************************************/
 
 
 
 int32_t main(){
     ios::sync_with_stdio(false); cin.tie(nullptr);
-    cout.precision(10); cout << fixed;
+    cout.precision(10);cout<<fixed;
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "rt", stdin);
+    freopen("input.txt","rt",stdin);
 #endif
 
     
  
 #ifndef ONLINE_JUDGE
-    cerr << "Time:" << 1.0*clock()/CLOCKS_PER_SEC << "s.\n";
+    cerr<<"Time:"<<1.0*clock()/CLOCKS_PER_SEC<<"s.\n";
 #endif
     return 0;
 }
