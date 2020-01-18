188e45d165009af2dd362b9a84102a0a8eaeafd5
Abhishek Chaturvedi <abhishekchaturvedi987@gmail.com>
2020-01-18 10:15:14 +0530

    Create newyearnaming.cpp

---
+++ codeforces/newyearnaming.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define eb emplace_back
+#define um unordered_map
+#define all(x) (x).begin(),(x).end()
+#define forn(i,n) for(int i=0;i<int(n);++i)
+#define for1(i,n) for(int i=1;i<=int(n);++i)
+#define ford(i,n) for(int i=(int)(n)-1;i>=0;--i)
+#define fore(i,a,b) for(int i=(int)(a);i<=(int)(b);++i)
+#define che(x) cout<<#x<<" having "<<x<<endl 
+using namespace std;
+typedef long double ld;typedef long long ll;
+typedef vector<int> vi;typedef vector<pair<int,int>> vpi;typedef vector<vi> vvi;
+typedef vector<ll> vll;typedef vector<pair<ll,ll>> vpll;typedef vector<vll> vvll;
+template<class T>bool uin(T &a,T b){return a>b?(a=b,true):false;}
+template<class T>bool uax(T &a,T b){return a<b?(a=b,true):false;}
+const ll MOD=1e9+7,INF=1e18;
+/********************************************************************************/
+ 
+ 
+ 
+int32_t main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    cout.precision(10);cout<<fixed;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","rt",stdin);
+#endif
+ 
+    int m,n; cin>>m>>n;
+    string a[m+1],b[n+1];
+    for1(i,m) cin>>a[i];
+    for1(i,n) cin>>b[i];
+    int q;
+    cin>>q;
+    while(q--){
+    	int p; cin>>p;
+    	if(p%m!=0) cout<<a[p%m];
+    	else cout<<a[m];
+    	if(p%n!=0) cout<<b[p%n];
+    	else cout<<b[n];
+    	cout<<endl;
+    }
+ 
+#ifndef ONLINE_JUDGE
+    cerr<<"Time:"<<1.0*clock()/CLOCKS_PER_SEC<<"s.\n";
+#endif
+    return 0;
+}
